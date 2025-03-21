8f6e6f6e468e94e0be85ea7a4b65d80914624271
Preetham B <bpreetham58@gmail.com>
2025-03-21 10:56:16 +0530

    login-signup-page-updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import AppRouter from "./Router"
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <AppRouter />
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Login />} /> {/* Default to Login page */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
